9b1a0ca532466f0d6edd5025d549fc58a8a6e6cc
Simon R Klaver <Justist@users.noreply.github.com>
2024-06-02 17:27:41 +0200

    Initial commit

---
+++ tests/companies.spec.ts
@@ -0,0 +1,67 @@
+import {test, expect, Page, BrowserContext} from '@playwright/test';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+
+let homepage : string = "https://www.linkedin.com/";
+
+// Create an interface to have the data from the user credentials yaml be interpretable
+interface LoginData {
+    username: string;
+    password: string;
+}
+
+// Parse the user credentials yaml file
+const credentialContents = fs.readFileSync('playwright/.auth/userCredentials.yaml', 'utf8');
+const credentialData = yaml.load(credentialContents) as LoginData[];
+// Only take the first entry
+const loginData = credentialData[0];
+
+let context: BrowserContext;
+
+// Before all tests, login to LinkedIn
+test.beforeAll("Setup", async ({ browser }) => {
+    context = await browser.newContext();
+    const page = await context.newPage();
+    await login(page, loginData.username, loginData.password);
+});
+
+// Create an interface to have the data from the yaml be interpretable
+interface CompanyData {
+    companyName: string;
+    followers: string;
+}
+
+// Parse the yaml file
+const companyContents = fs.readFileSync('test-input/companies.yaml', 'utf8');
+const companyData = yaml.load(companyContents) as CompanyData[];
+
+for (const companyEntry of companyData) {
+    test (`Check followers of company ${companyEntry.companyName}`, async ({ page }) : Promise<void> => {
+        await context.newPage(); // To make sure we are logged in
+        await goToCompanyPage(companyEntry.companyName)({page});
+        // Check only the text at the top of the page, otherwise there are many more (wrong) matches
+        let topElements = await page.$$(".org-top-card-summary-info-list__info-item");
+        let followerRegex = new RegExp(`(\\b\\w+\\b)(?=\\s+followers)`);
+        for (let element of topElements) {
+            let text = await element.textContent();
+            if (text.match(followerRegex)) {
+                expect(text.match(followerRegex)[0]).toBe(companyEntry.followers);
+                return;
+            }
+        }
+    });
+}
+
+async function login(page: Page, username: string, password: string) : Promise<void> {
+    await page.goto(homepage + "login");
+    await page.getByLabel('Email or Phone').fill(username);
+    await page.getByLabel('Password').fill(password);
+    await page.locator('button >> text="Sign in"').click();
+}
+
+function goToCompanyPage(company: string) {
+    return async ({ page }) : Promise<void> => {
+        await page.goto(homepage + 'company/' + company);
+        await expect(page).toHaveURL(new RegExp(`company/${company}`));
+    };
+}
---
+++ tests/login.spec.ts
@@ -0,0 +1,77 @@
+import {test, expect, Page} from '@playwright/test';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+
+let homepage : string = "https://www.linkedin.com/";
+
+// Create an interface to have the data from the user credentials yaml be interpretable
+interface LoginData {
+    username: string;
+    password: string;
+}
+
+// Parse the user credentials yaml file
+const credentialContents = fs.readFileSync('playwright/.auth/userCredentials.yaml', 'utf8');
+const credentialData = yaml.load(credentialContents) as LoginData[];
+// Only take the first entry
+const loginData = credentialData[0];
+
+// If we have multiple login data, we can use a loop to test all of them
+// Now we are only using the first one
+// for (const data of credentialData) {
+  test(`login with username ${loginData.username}`, async ({ page }) : Promise<void> => {
+    await login(page, loginData.username, loginData.password);
+    await expect(page).toHaveURL(/feed/);
+  });
+// }
+
+
+test('login with no e-mail', async ({ page }) : Promise<void> => {
+  await page.goto(homepage + 'login');
+
+  await page.getByLabel('Email or Phone').fill('wrongUserName');
+  await page.getByLabel('Password').fill('wrongPassword');
+  await page.locator('button >> text="Sign in"').click();
+  const pageContent = await page.textContent('body');
+  expect(pageContent.includes('Please enter a valid username')).toBe(true);
+});
+
+test('login with no valid e-mail', async ({ page }) : Promise<void> => {
+  await page.goto(homepage + 'login');
+
+  await page.getByLabel('Email or Phone').fill('wrongUserName@mail.com');
+  await page.getByLabel('Password').fill('wrongPassword');
+  await page.locator('button >> text="Sign in"').click();
+  const pageContent = await page.textContent('body');
+  expect(pageContent.includes('Wrong email or password. Try again or')).toBe(true);
+});
+
+test('forgot password', async ({ page }) : Promise<void> => {
+  await page.goto(homepage + 'login');
+  await page.content(); // Wait for the page to load so it works on Chromium
+  await page.locator('a.btn__tertiary--medium.forgot-password').click();
+
+  await expect(page).toHaveURL(/request-password-reset/);
+});
+
+test('login with third-party service', async ({ page }) : Promise<void> => {
+  await page.goto(homepage + 'login');
+  // TODO: Doesn't work for Chromium / Edge?? Needs debugging by someone who knows more about Playwright
+  let popup : Promise<Page> = page.waitForEvent('popup');
+  await page.locator('div[aria-label="Sign in with Google"]').click();
+  let thirdPartyLoginPage : Page = await popup;
+  await expect(thirdPartyLoginPage).toHaveURL(/accounts\.google\.com/);
+});
+
+test('sign up / join now', async ({ page }) : Promise<void> => {
+  await page.goto(homepage + '/login');
+  await page.click('a[id="join_now"]');
+  await expect(page).toHaveURL(/signup/);
+});
+
+async function login(page: Page, username: string, password: string) : Promise<void> {
+  await page.goto(homepage + "login");
+  await page.getByLabel('Email or Phone').fill(username);
+  await page.getByLabel('Password').fill(password);
+  await page.locator('button >> text="Sign in"').click();
+}
